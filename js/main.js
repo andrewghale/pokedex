5d6fdb947390a5d3cf17fa6f37111c79f5e1b910
Andrew Hale <andyhalebmx@hotmail.com>
2020-02-19 22:20:05 +0000

    styled buttons, new screenshot

--- js/main.js
+++ js/main.js
@@ -2,7 +2,16 @@ jQuery(document).ready(function($){
 
   const number = document.getElementById('numberField')
   const button = document.getElementById('submitnumber')
+  const randomButton = document.getElementById('submitrandom')
 
+
+  randomButton.addEventListener('click', function(e) {
+    e.preventDefault()
+    // number below is 806 because there 807 pokemon
+    const randomInput = Math.floor(Math.random()*806)+1
+    pokeApi(randomInput)
+    document.getElementById('numberField').value = randomInput
+  })
 
   button.addEventListener('click', function(e) {
     e.preventDefault()
@@ -33,7 +42,7 @@ jQuery(document).ready(function($){
   }
 
   const generateHtml = (data) => {
-    const { name, sprites, height, weight } = data;
+    const { name, sprites, height, weight, id } = data
     const html =
       `
       <h1 class="name">${capitalizeFirst(name)}</h1>
@@ -42,17 +51,18 @@ jQuery(document).ready(function($){
           <img src=${sprites.front_default} alt=${capitalizeFirst(name)}>
         </div>
         <div class="image-container">
-          <img src=${sprites.back_default} alt=${capitalizeFirst(name)}>
+          <img src=${!sprites.back_default ? sprites.front_default : sprites.back_default} alt=${capitalizeFirst(name)}>
         </div>
       </div>
       <div class="details">
         <span>Height: ${height}</span>
         <span>Weight: ${weight}</span>
         <span>${getTypes(data).length >= 2 ? 'Types&nbsp;' : 'Type&nbsp;&nbsp;'}: ${getTypes(data).join(', ')}</span>
+        <span>Number: ${id}</span>
       </div>
       `
     const pokemonDiv = document.querySelector('.pokemon')
     pokemonDiv.innerHTML = html
-    slickInit();
+    slickInit()
   }
-});
+})
